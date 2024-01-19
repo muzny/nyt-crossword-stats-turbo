6e24f9dfa99564b802cfb0281e53ec43999b4f44
Felix Muzny <f.muzny@northeastern.edu>
2024-01-19 12:25:31 -0500

    working to get full data, not cleaned up yet

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-1. Set your NYT_COOKIE to your correct cookie value:
-`javascript:document.cookie.split('; ').find(r=>r.startsWith('NYT-S')).split('=')[1]` in an inspector for a page you are logged in on
+Running
+---
+```
+python get_stats.py [--outfile outfile.csv] [--kind [summaries|full]] --cookie 'your cookie here in single quotes'
+```
+
+Default value for `--outfile` is `data.csv`, default value for `--kind` is `summaries`.
+
+To get your cookie value (this will be a long string), here is one strategy: 
+1. Log in to NYT Crosswords via a website
+2. Open the page inspector (on Firefox, "Inspect")
+3. Click the "Network" tab
+4. Find a GET request to `www.nytimes.com` that looks like `crosswords?login=email&auth=login-email`, click this
+5. Under "Request Headers", look for the field `Cookie`
+6. Copy+paste your entire cookie (as an example, mine is 2527 characters long)
+
+Endpoints & Data
+---
+From the general puzzle id & info endpoint (`https://www.nytimes.com/svc/crosswords/v3/36569100/puzzles.json?date_start={start date}&date_end={end date}&publish_type=daily`, where start and end dates are in format `yyyy-mm-dd`), we get results in the format:
+
+```
+{"status":"OK","results":[{"author":"Gia Bosko","editor":"Will Shortz","format_type":"Normal","print_date":"2023-04-10","publish_type":"Daily","puzzle_id":20953,"title":"","version":0,"percent_filled":0,"solved":false,"star":null},{"author":"John Ewbank","editor":"Will Shortz","format_type":"Normal","print_date":"2023-04-09","publish_type":"Daily","puzzle_id":20961,"title":"If the Clue Fits ...","version":0,"percent_filled":100,"solved":true,"star":"Gold"}]}
+```
+Note that this endpoint is accessible without providing your cookie. In this case, you'll get less info (the last puzzle does not indicate solved in this case):
+
+```
+{"status":"OK","results":[{"author":"Gia Bosko","editor":"Will Shortz","format_type":"Normal","print_date":"2023-04-10","publish_type":"Daily","puzzle_id":20953,"title":"","version":0,"percent_filled":0,"solved":false,"star":null},{"author":"John Ewbank","editor":"Will Shortz","format_type":"Normal","print_date":"2023-04-09","publish_type":"Daily","puzzle_id":20961,"title":"If the Clue Fits ...","version":0,"percent_filled":0,"solved":false,"star":null}]}
+```
+
+From the solve info endpoint, which requires the correct cookie (`https://www.nytimes.com/svc/crosswords/v6/game/20961.json`):
+
+```
+{"board":{"cells":[{"guess":"F","timestamp":1},...]},
+"calcs":{"percentFilled":100,"secondsSpentSolving":1834,"solved":true},"firsts":{"cleared":1681057721,"opened":1681057643,"solved":1681059477},"lastCommitID":"xxxxx","puzzleID":20961,"timestamp":1681059478,"userID":XXXXXX,"minGuessTime":1681057686,"lastSolve":1681059477}
+```
+If you do not pass your cookie you will get an ERROR.
+
+The fields under "firsts" do not use milliseconds since epoch (`https://currentmillis.com/`), but I'm currently unsure what unit they do use.
+
+"board" format
+------
+Cell 0 (the cell at index 0 in the "cells" list) is in the upper left.
+Cell 1 is to the right of cell 0.
+Cells that are blocked/black are marked as "blank": true.
+Cells that have a guess have the fields "guess": "letter" and "timestamp" : number of seconds elapsed since puzzle start when it was entered.
+
+On a Sunday crossword (21 x 21), cell 21 is the first cell in the second row, cell 42 is the first cell in the third row, etc. Cell 440 is the last cell in the lower right.
+
+On a daily crossword (15 x 15), cell 15 is the first cell in the second row, cell 30 is the first cell in the third row, etc. Cell 224 is the last cell in the lower right.
+
+Every so often (e.g., "2023-09-19") there is a puzzle that is neither 21 x 21 nor 15 x 15 (e.g., 14 x 16 has 224 cells instead of 225). This makes us annoyed.
+
+
+Other resources/notes
+---
+Some other resources say that you only need the `NYT-S` part of the cookie, but I haven't been able to get that to work yet.
+
+https://github.com/kesyog/crossword has a rust library for getting data from the NYT crossword enpoints.
+
+https://github.com/michadenheijer/pynytimes has a great general NYT API library in python.
+
+
--- get_stats.py
+++ get_stats.py
@@ -1,18 +1,26 @@
 import requests
 import json
 import datetime
-from datetime import date
 import pandas as pd
 # from tqdm import tqdm
 import argparse
 
 
-# A = argparse.ArgumentParser(
-#     description='Gets data from NYT crosswords for puzzles',
-#     formatter_class=argparse.ArgumentDefaultsHelpFormatter
-# )
-# A.add_argument('-o', '-out_file', action='store', type=str,
-#     help="The output path where data should be saved (e.g. out.csv)")
+A = argparse.ArgumentParser(
+    description='Gets data from NYT crosswords for puzzles',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+A.add_argument('-c', '--cookie', action='store', type=str,
+    help="Your full cookie from NYT (e.g. nyt-a=...), enclosed in single quotes")
+A.add_argument('-o', '--outfile', action='store', type=str, default="data.csv",
+    help="The output path where data should be saved (e.g. data.csv)")
+A.add_argument('-v', '--verbose', action='store_true')
+A.add_argument('-k', '--kind', action='store', type=str, default="summaries",
+               help="The kind of data to get ('summaries' or 'full')")
+A.add_argument('-s', '--start', action='store', type=str, 
+               help="Date to start gathering data from (inclusive) in format YYYY-MM-DD")
+A.add_argument('-e', '--end', action='store', type=str, 
+               help="Date to stop gathering data from (exclusive) in format YYYY-MM-DD")
 
 
 # https://github.com/kesyog/crossword
@@ -23,13 +31,16 @@ import argparse
 # have to ask for 100 days at a time
 
 # params: ?publish_type=daily&date_start=2023-01-01&date_end=2024-01-13
-
 PUZZLE_IDS_ENDPOINT = "https://www.nytimes.com/svc/crosswords/v3/36569100/puzzles.json"
 
-# To fetch solve stats for a given puzzle, send a GET request as below, replacing {id} with the puzzle id. This API requires a NYT crossword subscription. {subscription_header} can be found by snooping on outgoing HTTP requests via Chrome/Firefox developer tools while opening a NYT crossword in your browser. Alternatively, you can supposedly extract your session cookie from your browser and send that instead (see linked reddit post below), but I haven't tried it myself.
+# To fetch solve stats for a given puzzle, send a GET request as below, replacing {id} with the puzzle id. 
+# This API requires a NYT crossword subscription. {subscription_header} can be found by snooping on outgoing 
+# HTTP requests via Chrome/Firefox developer tools while opening a NYT crossword in your browser. 
+# Alternatively, you can supposedly extract your session cookie from your browser and send that instead 
+# (see linked reddit post below), but I haven't tried it myself.
 GAME_SOLVE_ENDPOINT = "https://www.nytimes.com/svc/crosswords/v6/game/"
 
-# curl 'https://www.nytimes.com/svc/crosswords/ v3/36569100/puzzles.json?publish_type=daily&date_start={start_date}&date_end={end_date}' -H 'accept: application/json'
+
 
 def get_day_incs(start_date, end_date, inc = 100) -> list:
 
@@ -48,25 +59,30 @@ def get_day_incs(start_date, end_date, inc = 100) -> list:
 
     return dates
 
-def get_puzzle_info(start_date: str, end_date: str, cookie: str = None, publish_type: str = "daily") -> list:
-    headers = {'content-type': 'application/json'}
+def get_puzzle_ids(start_date: str, end_date: str, cookie: str = None, publish_type: str = "daily") -> list:
     # this endpoint will  work with no 'Cookie', but it won't get you as much information
+    headers = {'content-type': 'application/json',
+               'Cookie': cookie}
+    
     payload = {'date_start': start_date, 'date_end': end_date, 'publish_type': publish_type}
+
     r = requests.get(PUZZLE_IDS_ENDPOINT, headers = headers, params = payload)
-    # print(r.url)
+    print(r.url)
     results = r.json()['results']
 
     return results
 
     
 
-def get_solve_info(results: list, cookie: str) -> list:
+def get_solve_info(results: list, cookie: str, summaries: bool = True) -> list:
     headers = {'content-type': 'application/json',
                'Cookie': cookie}
     # this endpoint will not work with no cookie
 
     # endpoint ends with {id}.json
     timed_puzzles = []
+    if results is None:
+        return timed_puzzles
     for id_res in results:
         # skip ones that you've filled nothing in for
         if id_res['percent_filled'] == 0:
@@ -74,16 +90,34 @@ def get_solve_info(results: list, cookie: str) -> list:
     
         id = id_res['puzzle_id']
         r = requests.get(GAME_SOLVE_ENDPOINT + str(id) + ".json", headers = headers)
+        
+        print(r.url)
+        print(id_res["print_date"])
+        board = r.json()['board']['cells']
         calcs = r.json()['calcs']
         if len(calcs) > 0:
             # {'percentFilled': 100, 'secondsSpentSolving': 886, 'solved': True}
             copy_res = id_res.copy()
             copy_res.update(calcs)
+            if not summaries:
+                blank_cells = [ind for ind in range(len(board)) if "blank" in board[ind]]
+                guess_cells = [board[ind]["guess"] if "guess" in board[ind] else False for ind in range(len(board))]
+                time_cells = [board[ind]["timestamp"] if "timestamp" in board[ind] else False for ind in range(len(board))]
+                # blank_cells = {'cell' + str(ind) + "_blank": True if "blank" in board[ind] else False for ind in range(len(board))}
+                # guess_cells = {'cell' + str(ind) + "_guess": board[ind]["guess"] if "guess" in board[ind] else False for ind in range(len(board))}
+                # time_cells = {'cell' + str(ind) + "_timestamp": board[ind]["timestamp"] if "timestamp" in board[ind] else False for ind in range(len(board))}
+                # cells = {ind: cells[ind] for ind in range(len(cells))}
+                print(len(blank_cells), len(guess_cells), len(time_cells))
+                copy_res["blanks"] = blank_cells
+                copy_res["guesses"] = guess_cells
+                copy_res["times"] = time_cells
+                # copy_res.update(time_cells)
+                # copy_res['cells'] = board
             timed_puzzles.append(copy_res)
     
     return timed_puzzles
 
-def get_complete_info(start_date: str, end_date: str, cookie: str, verbose = True) -> pd.DataFrame:
+def get_complete_info(start_date: str, end_date: str, cookie: str, summaries: bool = True, verbose = True) -> pd.DataFrame:
     date_incs = get_day_incs(start_date, end_date)
     all_results = []
     for i in range(len(date_incs) - 1):
@@ -91,17 +125,18 @@ def get_complete_info(start_date: str, end_date: str, cookie: str, verbose = Tru
         # exclusive end for no repeats
         end = datetime.datetime.strftime(date_incs[i + 1] - datetime.timedelta(days=1), "%Y-%m-%d")
         print(start, "->", end)
-        results = get_puzzle_info(start, end, cookie)
-        results_time = get_solve_info(results)
+        results = get_puzzle_ids(start, end, cookie)
+        results_time = get_solve_info(results, cookie, summaries)
         all_results += results_time
 
-    print(len(all_results))
+    print("Number of puzzles with data recorded: ", len(all_results))
     return pd.DataFrame(all_results)
 
-cookie = 
-df = get_complete_info("2023-01-01", "2024-01-14", cookie)
-print(df.head())
-# write it to a csv
-df.to_csv("data.csv")
-# results = get_puzzle_info("2023-01-01", "2023-03-01")
-# results_time = get_solve_info(results)
+if __name__ == "__main__":
+    args = A.parse_args()
+    cookie = args.cookie
+    print("Cookie length: ", len(cookie))
+    df = get_complete_info(args.start, args.end, cookie, args.kind == "summaries")
+    print(df.head())
+    # write it to the specified file
+    df.to_csv(args.outfile, index = False)
